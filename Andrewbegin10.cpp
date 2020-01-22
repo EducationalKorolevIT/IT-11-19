79112a08133df0f88966d0dab188ebb98f207ca7
проход закрыт < >
2020-01-22 19:16:16 +0300

    Create Andrewbegin10.cpp

---
+++ Andrewbegin10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int a = 0;
+    cin >> a;
+    int b=0;
+    cin >> b;
+    a=pow(a,2);
+    b=pow(b,2);
+    cout << a+b << endl << a-b <<endl<< a/b << endl << a*b;
+}
