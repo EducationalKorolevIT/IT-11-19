06c52a1c0238410927ec2286edb5cb1481db9542
KaRaPuZuK <bogdanovand3909@mail.ru>
2020-01-24 19:22:51 +0300

    add

---
+++ src/For12.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class For12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float d = 0;
+        float N = sc.nextFloat();
+
+        for (i = 1, i > 0, i = i + 1)
+
+
+    }
+}
